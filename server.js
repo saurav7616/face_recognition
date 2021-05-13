da585ef640a38a4caff090070203c0576b0f42ff
saurav kumar <ksaurav7616@gmail.com>
2021-05-13 16:07:13 +0530

    backend files

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
+
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'postgre123',
+      database : 'imgrecodb'
+    }
+ });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());	
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegs(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageUrl', (req, res) => { image.handleApi(req, res)})
+
+app.listen(3000, () => {
+	console.log('runnning fine on port 3000');
+})
